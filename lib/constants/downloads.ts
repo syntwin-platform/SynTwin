d2a38e02a91fc3cdbf6654f5bc749d05435d7993
quanganhse184436 <anhndqse184436@fpt.edu.vn>
2026-07-31 15:12:27 +0700

    a
    a

--- lib/constants/downloads.ts
+++ lib/constants/downloads.ts
@@ -1,9 +1,10 @@
 export const DEFAULT_FAIROBOT_DOWNLOAD_URL =
-    "/downloads/FaiRobot-Studio-1.0.1-Setup.exe";
+    "https://storage.googleapis.com/syntwin-staging-reso-2026-fairobot-downloads/FaiRobot-Studio-1.0.0-Setup.exe";
 
 export const FAIROBOT_DOWNLOAD_URL =
     process.env.NEXT_PUBLIC_FAIROBOT_DOWNLOAD_URL || DEFAULT_FAIROBOT_DOWNLOAD_URL;
 
 export const FAIROBOT_VERSION = "1.0.1";
+
 
 
